b42eea46160cfa11eed71a7873b05a54b568e6a2
yk0826 <616698388@qq.com>
2021-04-25 17:20:54 +0800

    第一次提交

---
+++ src/main/java/com/yk/hello/a/TestB.java
@@ -0,0 +1,30 @@
+package com.yk.hello.a;
+
+import java.util.concurrent.Callable;
+
+/**
+ * 测试
+ */
+public class TestB extends TestA implements Callable {
+
+    @Override
+    public void show() {
+        System.out.println("hello world");
+    }
+
+    public static void main(String[] args) {
+        TestA c = new TestA() {
+            @Override
+            public void show() {
+
+            }
+        };
+        TestA a = new TestB();
+        a.show();
+    }
+
+    @Override
+    public Object call() throws Exception {
+        return null;
+    }
+}
